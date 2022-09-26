92d1d43bd983f12aa3d009145dfc6b800334fa2f
Tim D <timvandeutekom@hotmail.com>
2022-09-26 10:41:02 +0200

    adds the option to pick the amount of squares

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
-const container = document.querySelector('#container');
+    const container = document.querySelector('#container');
 const buttonGrid = document.querySelector('#buttonGrid')
 let userInput = 16
 let totalSquares = userInput * userInput
 
+createSquares()
 
-for(let i=0; i< totalSquares; i++){
-    const content = document.createElement('div');
-    content.classList.add('content');
-    container.appendChild(content);
 
-    content.addEventListener('mouseover',function(){
-    content.style.backgroundColor="red";
-    });
-    // content.addEventListener('mouseleave',function(){
-    //     content.style.backgroundColor="blue";
-    //     });
+function createSquares(){
+    removeSquares()
+    for(let i=0; i< totalSquares; i++){
+        
+        let content = document.createElement('div');
+        content.classList.add('content');
+        container.appendChild(content);
+
+        content.addEventListener('mouseover',function(){
+        content.style.backgroundColor="red";
+        });
+    }
 }
 
 buttonGrid.addEventListener('click', function(){
-   userInputString = prompt('Number of squares per side?')
-   userInput = parseInt(userInputString);
-   return userInput
+    userInputString = prompt('Number of squares per side?')
+    if  (userInputString > 0 && userInputString < 51){
+        userInput = parseInt(userInputString);
+        totalSquares = userInput * userInput
+        createSquares()
+        gridTemplate()
+    } else userInputString = alert('Please select a number between 1-50')
 })
 
-// container.style.setProperty('grid-template-columns', 'repeat(' + userInput + ', 1fr)');
+function removeSquares() {
+    while(container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+
+function gridTemplate(){
+container.style.setProperty('grid-template-columns', 'repeat(' + userInput + ', 1fr)');
+}
+
+
