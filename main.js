712c3485368aab1c7e7a5bcf4c70805e881ca4b1
Tim D <timvandeutekom@hotmail.com>
2022-09-27 11:28:36 +0200

    adds eraser button

--- main.js
+++ main.js
@@ -2,82 +2,90 @@ const container = document.querySelector('#container');
 const buttonGrid = document.querySelector('#buttonGrid')
 const buttonRainbow = document.querySelector('#buttonRainbow')
 const buttonBlack = document.querySelector('#buttonBlack')
+const buttonEraser = document.querySelector('#buttonEraser')
 const buttonReset = document.querySelector('#buttonReset')
 let userInput = 16
 let totalSquares = userInput * userInput
 let buttonRainbowPressed = 0
+let buttonEraserPressed = 0
 
 createSquares()
 
-
 function createSquares(){
-    removeSquares()
-    for(let i=0; i< totalSquares; i++){
-        
-        let content = document.createElement('div');
-        content.classList.add('content');
-        container.appendChild(content);
+  removeSquares()
+  for(let i=0; i< totalSquares; i++){
+    let content = document.createElement('div');
+    content.classList.add('content');
+    container.appendChild(content);
 
-        content.addEventListener('mouseover',function(){
-
-
-        if (buttonRainbowPressed === 1){
-        content.style.setProperty('background-color', randomRGB())
-        } else { content.style.backgroundColor="black";}
-        });
-        buttonBlack.addEventListener('click', function(){
-            buttonRainbowPressed = 0
-            buttonBlack.style.cssText = "box-shadow: 0px 0px 15px 5px orange;"
-            buttonRainbow.style.cssText = ""
-            
-            // if (typeof content !== 'undefined') {
-            //     content.style.backgroundColor="white";
-            // }
-            
-        })
-    }
+    content.addEventListener('mouseover',function(){
+    if (buttonRainbowPressed === 1){
+      content.style.setProperty('background-color', randomRGB())
+      } else if (buttonEraserPressed === 1){
+      content.style.backgroundColor="white";
+      } else { 
+      content.style.backgroundColor="black";
+      }
+    });
+  }
 }
 
 buttonGrid.addEventListener('click', function(){
-    userInputString = prompt('Number of squares per side?')
-    if  (userInputString > 0 && userInputString < 51){
-        userInput = parseInt(userInputString);
-        totalSquares = userInput * userInput
-        createSquares()
-        gridTemplate()
-    } else userInputString = alert('Please select a number between 1-50')
+  userInputString = prompt('Number of squares per side?')
+  if (userInputString > 0 && userInputString < 51){
+      userInput = parseInt(userInputString);
+      totalSquares = userInput * userInput
+      createSquares()
+      gridTemplate()
+  } else userInputString = alert('Please select a number between 1-50')
 })
 
 function removeSquares() {
-    while(container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
   }
+}
 
 function gridTemplate(){
-container.style.setProperty('grid-template-columns', 'repeat(' + userInput + ', 1fr)');
+  container.style.setProperty('grid-template-columns', 'repeat(' + userInput + ', 1fr)');
 }
 
 
 function randomRGB(){
-    function randomNumber(){
-      return Math.floor(Math.random()*255);
-    }
-    let r = randomNumber()
-    let g = randomNumber()
-    let b = randomNumber()
-    let rgb = `rgb(${r},${g},${b})`
-    return rgb
+  function randomNumber(){
+    return Math.floor(Math.random()*255);
+  }
+  let r = randomNumber()
+  let g = randomNumber()
+  let b = randomNumber()
+  let rgb = `rgb(${r},${g},${b})`
+  return rgb
 }
 
 
-buttonRainbow.addEventListener('click', function(){
-    buttonRainbowPressed = 1
-    buttonRainbow.style.cssText = "box-shadow: 0px 0px 15px 5px orange;"
-    buttonBlack.style.cssText = ""
+buttonBlack.addEventListener('click', function(){
+  buttonRainbowPressed = 0
+  buttonEraserPressed = 0
+  buttonBlack.style.cssText = "box-shadow: 0px 0px 15px 5px orange;"
+  buttonRainbow.style.cssText = ""
 })
 
+buttonRainbow.addEventListener('click', function(){
+  buttonRainbowPressed = 1
+  buttonEraserPressed = 0
+  buttonRainbow.style.cssText = "box-shadow: 0px 0px 15px 5px orange;"
+  buttonBlack.style.cssText = ""
+  buttonEraser.style.cssText = ""
+})
+
+buttonEraser.addEventListener('click', function(){
+  buttonEraserPressed = 1
+  buttonRainbowPressed = 0
+  buttonEraser.style.cssText = "box-shadow: 0px 0px 15px 5px orange;"
+  buttonBlack.style.cssText = ""
+  buttonRainbow.style.cssText = ""
+})
 
 buttonReset.addEventListener('click', function(){
-    document.location.reload()
+  document.location.reload()
 })
